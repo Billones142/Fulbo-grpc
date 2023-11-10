74b1ddf26d2916f7e567e54c76f9cdaf60a5c493
Billones142 <stefanomerinoderui@gmail.com>
2023-11-09 21:00:11 -0300

    fucionando comunicacion y menu simple

--- grpc-client/src/main/java/com/grpcCliente/servicioFulbo/Cliente.java
+++ grpc-client/src/main/java/com/grpcCliente/servicioFulbo/Cliente.java
@@ -7,16 +7,91 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.grpcInterfaces.fulbo.ChatServiceGrpc;
 import com.grpcInterfaces.fulbo.Peticion;
 import com.grpcInterfaces.fulbo.RecibirMensaje;
+import com.grpcInterfaces.fulbo.SeleccionAFA_gRPC;
+import com.grpcInterfaces.fulbo.SeleccionAFA_gRPC.Builder;
 
+import fulbo.ucp.SeleccionAFA;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 
 
 @SpringBootApplication
 public class Cliente {
-
 	private Channel channel;
     private ChatServiceGrpc.ChatServiceBlockingStub blockingStub;
+
+	Builder seleccionAFA;
+
+	/******************************Inicio de menus******************************/
+
+	public void menuPrincipal() {
+		// TODO: agregar que resetee la terminal
+		
+		enviarMensaje("ping"); // ping para verificar la coneccion con el servidor
+
+        System.out.println("Elija una opcion\r\n" + //
+                            "1. editar seleccion AFA (si ya existe)\r\n" + //
+                            "2. crear nueva Seleccion AFA\r\n" + //
+                            "3. enviar Ping\n\r" + //
+							"4. terminar comunicacion con el servidor");
+        int eleccion= Integer.parseInt(System.console().readLine());
+
+		switch (eleccion) {
+			case 1:
+				menuEditarSeleccion();
+				break;
+
+			case 2:
+				menuCrearSeleccion();
+				break;
+
+			case 3:
+				ping();
+				menuPrincipal();
+				break;
+
+			case 4:
+				break;
+		
+			default:
+				System.out.println("No selecciono una opcion valida");
+				menuPrincipal();
+				break;
+		}
+    }
+
+	public void menuEditarSeleccion(){
+		System.out.println();
+	}
+
+	public void menuCrearSeleccion() { //TODO
+		Builder nuevaSeleccion= SeleccionAFA_gRPC.newBuilder();
+
+		System.out.println("Cual es el apellido de su presidente?");
+		String apellidoPresidente= System.console().readLine();
+		nuevaSeleccion.setPresidente(apellidoPresidente);
+
+		System.out.println("quieres agregar un integrante? \r\n" + //
+							"1. Si\r\n" + //
+							"2. No");
+		int eleccionAgregrarIntegrante= Integer.parseInt(System.console().readLine());
+		if (eleccionAgregrarIntegrante == 1) {
+			//menuAgregarIntegrante();
+		}
+
+		System.out.println("termino de ingresar los datos para su nueva seleccion, quiere enviarla al servidor?\r\n" + //
+							"1. Si\r\n" + //
+							"2. No");
+		int eleccionEnviarSeleccion= Integer.parseInt(System.console().readLine());
+
+
+		if (eleccionEnviarSeleccion == 1) {
+			seleccionAFA= nuevaSeleccion;
+			blockingStub.enviarSeleccion(nuevaSeleccion.build());
+		}
+	}
+
+	/******************************Fin de menus******************************/
 
     public Cliente(String host, int port) {
 		this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
@@ -24,17 +99,17 @@ public class Cliente {
 	}
 
 	public void ping() {
-		RecibirMensaje respuestaPing= this.sendMessage("ping");
-		System.out.println(respuestaPing.getMessage());
+		System.out.println("enviando Ping...");
+		RecibirMensaje respuestaPing= this.enviarMensaje("ping");
+		System.out.println("respuesta del servidor: " + respuestaPing.getMessage());
 	}
 
-    public RecibirMensaje sendMessage(String message) {
+    public RecibirMensaje enviarMensaje(String message) {
         Peticion peticion = Peticion.newBuilder()
 		.setTo(1)
 		.setMessage(message)
 		.build();
 
-        RecibirMensaje respuesta = blockingStub.ping(peticion);
-		return respuesta;
+		return blockingStub.ping(peticion);
     }
 }
--- grpc-client/src/main/java/com/grpcCliente/servicioFulbo/Main.java
+++ grpc-client/src/main/java/com/grpcCliente/servicioFulbo/Main.java
@@ -1,13 +1,25 @@
 package com.grpcCliente.servicioFulbo;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        Cliente client = new Cliente("localhost", 6999);
-        System.out.println("haciendo ping...");
-        client.ping();
-		System.console().readLine();
+        while (true) {
+            try {
+                Cliente client = new Cliente("localhost", 6999);
+                client.menuPrincipal();
+            } catch (Exception e) {
+                System.out.print("no se pudo establecer la coneccion con el servidor\r\n" + //
+                "reintentado en 3 segundos");
+                try {
+                    for (int i = 0; i < 3; i++) {
+                        System.out.print(".");
+                        Thread.sleep(1000);
+                    }
+                    } catch (InterruptedException a) {
+                    Thread.currentThread().interrupt();
+                    }
+                    System.out.println("\r\n");
+            }
+        }
     }
 }
